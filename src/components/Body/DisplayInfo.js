bb93d628a22ebd3d67c29a1a47b72640c8e3341f
Omarfaruk05 <mdomarfrauk149518.com>
2022-07-14 00:41:44 +0600

    add send email functionality

--- src/components/Body/DisplayInfo.js
+++ src/components/Body/DisplayInfo.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { XIcon, PencilIcon } from '@heroicons/react/outline'
 import { toast } from 'react-toastify';
 
 const DisplayInfo = ({info, refetch}) => {
+    const [agree, setAgree] = useState(false);
     const {_id, name, phoneNumber, email, hobbies} =  info;
 
     const handleDelete = () => {
@@ -21,11 +22,25 @@ const DisplayInfo = ({info, refetch}) => {
                 toast.error('Failed to Delete')
             }
         })
+    };
+
+    const handleEmail =(_id) => {
+        fetch('http://localhost:5000/email', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(info)
+        })
+        .then(res => res.json())
     }
 
     return (
         <tr>
-            <th><input type="checkbox" className="checkbox" /> </th>
+            <th className='flex p-6'>
+                <input onClick={() => setAgree(!agree)} type="checkbox" className="checkbox mr-1"  />
+                <button disabled={!agree} onClick={handleEmail} className='btn btn-xs btn-success text-white'>Send Email</button> 
+            </th>
             <td>{name}</td>
             <td>{email}</td>
             <td>{phoneNumber}</td>
